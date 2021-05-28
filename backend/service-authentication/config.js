c52975ff46387bb4dc6f4c21b4294a37da7a60fd
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-28 21:01:52 +0200

    IP change to 127.0.0.1

--- backend/service-authentication/config.js
+++ backend/service-authentication/config.js
@@ -1,7 +1,7 @@
 const config = {
     // paramètres de connexion à la base de données
     mongodbDatabase: 'chat',
-    mongodbHost:     'mongodb://mongodb-authentication:27017/',
+    mongodbHost:     'mongodb://127.0.0.1:27017/',
     charset:       'utf8',
     mongodbLogin:  '',
     mongodbPassword: '',
