0f2a22c1e800d7c7ef53b24d61f861ef69619dca
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-28 16:02:05 +0200

    add parenthesis

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {JSdata, MessageService} from "../message/message.service";
-import {HttpClient} from "@angular/common/http";
-
-
 
 @Injectable({
   providedIn: 'root'
@@ -14,29 +11,29 @@ export class AuthService {
   // @ts-ignore
   islog: boolean;
 
+  constructor(private messageService: MessageService) { }
 
-  constructor(
-    private http: HttpClient,
-    private MS: MessageService
-  ) {}
+  getLogged(): boolean {
+    return this.islog;
+  }
+
+  setLogged(value: boolean): void {
+    this.islog = value;
+  }
 
   sendAuthentication(login: string, password: string): Observable<JSdata> {
-    const data = new FormData();
-    if (login !== null && login !== undefined && password !== null && password !== undefined) {
-      data.append(login, password);
-    }
-    return this.MS.sendMessage('checkLogin', {
-      login : login,
-      Password: password
+    return this.messageService.sendMessage('checkLogin', {
+      'login': login,
+      'password': password
     });
   }
 
-  finalizeAuthentication(data: JSdata): void{
-    if (data.status === 'ok') {
-      this.islog = true;
-    } else {
-      this.islog = false;
+  finalizeAuthentication(data: JSdata): void {
+    if (data.status === 'ok'){
+      this.setLogged(true);
+    }
+    else{
+      this.setLogged(false);
     }
   }
-
 }
