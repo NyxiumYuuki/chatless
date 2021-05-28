11e51a80db39c3fc836708f7dc7c327116984a30
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-28 21:08:33 +0200

    cors added + session working

--- backend/service-message/server.js
+++ backend/service-message/server.js
@@ -8,22 +8,22 @@ const bodyParser = require ('body-parser');
 const {sendError, sendMessage} = require ('./message');
 const messages = require('./mongodb-message');
 
+
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-const port = process.env.PORT || 3000;
+const io = new Server(server, {
+    cors: {
+    origin: "http://127.0.0.1:4200",
+    methods: ["GET", "POST"],
+    credentials: true
+    }
+});
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors({origin: 'http://127.0.0.1:4200', credentials: true}));
 app.use(cookieParser());
-io.use(function(socket, next){
-    const session = auth.getSession(socket.request);
-    const getUsername = auth.getUsername(session);
-    if (getUsername === -1) {
-        //sendError(res, 'not authenticated');
-    }
-    auth.setSessionCookie(socket.request, socket.request.res || {}, next);
-});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -34,20 +34,24 @@ io.on('connection',socket => {
 
     const session = auth.getSession(socket.request);
     const getUsername = auth.getUsername(session);
+    if (getUsername === -1) {
+        return sendError(socket, 'not authenticated');
+    }
 
     console.log(`${getUsername} joined the chat.`);
-    socket.broadcast.emit('general',`${getUsername} joined the chat.`);
+    socket.emit('general',`${getUsername} joined the chat.`);
     users[socket.id] = getUsername;
     messages.find({},(err, res) => {
         if(err) throw err;
         if(res.length > 0){
             const savedChat = res;
+            console.log(res);
             socket.emit('general',savedChat);
         }
     });
 
     socket.on('general',function(data){
-        socket.broadcast.emit('general',data);
+        socket.emit('general',data);
 
         const username = data.username;
         const date = Date.now();
@@ -73,11 +77,11 @@ io.on('connection',socket => {
     })
 
     socket.on("disconnect", function() {
-        console.log(`${socket.id} left the chat.`);
+        console.log(`${getUsername} left the chat.`);
     });
 
 });
 
 server.listen(port, () => {
-    console.log('listening on *:3000');
+    console.log(`listening on *:${port}/`);
 });
