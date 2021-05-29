34070afc0edef5827d66e3e9812546490a8f7c5f
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-29 14:41:35 +0200

    Auth is now working with keys

--- backend/service-authentication/auth.js
+++ backend/service-authentication/auth.js
@@ -1,30 +1,21 @@
 const sessionJwt = require ('./sessionJWT');
 const queries = require('./mongodbQueries');
 
-// ici, on récupère le contenu du cookie de session JWT.
-// celui-ci contient le userId mais également des informations
-// concernant sa date d'expiration.
-function getSession (req) {
-    return sessionJwt.decodeSessionCookie(req);
+function getSession (sessionid,res) {
+    return sessionJwt.decodeSessionCookie(sessionid,res);
 }
 module.exports.getSession = getSession;
 
-// cette fonction ajoute le cookie de session au headers du
-// message qui sera renvoyé à Angular. Si le cookie actuel
-// est "vieux", on en recrée ici un nouveau.
 function setSessionCookie (req, res, session) {
     sessionJwt.createSessionCookie(req, res, session);
 }
 module.exports.setSessionCookie = setSessionCookie;
 
-// fonction pour récupérer le userId provenant du cookie
-// de session. Si ce dernier n'existe pas, on renvoie
-// l'ID -1.
-function getUserId(session) {
+function getUsername(session) {
     if (typeof session.username === 'undefined') return -1;
     return session.username;
 }
-module.exports.getUserId = getUserId;
+module.exports.getUserId = getUsername;
 
 async function authenticate(req, res) {
     const login = req.body.login;
