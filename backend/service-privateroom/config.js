6818e0bb47b8b12cb10faa0cea2cc105076b340c
vankhaiphan <pvkhai98dn@gmail.com>
2021-05-30 20:56:42 +0200

    change port mongodbHost for service-privateroom

--- backend/service-privateroom/config.js
+++ backend/service-privateroom/config.js
@@ -1,6 +1,6 @@
 const config = {
     mongodbDatabase: 'chat',
-    mongodbHost:     'mongodb://127.0.0.1:27017/',
+    mongodbHost:     'mongodb://127.0.0.1:27018/',
     charset:       'utf8',
     mongodbLogin:  '',
     mongodbPassword: '',
