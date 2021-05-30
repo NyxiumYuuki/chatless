68f48b0c09bcbfadc26c41bedcda34083e23ee15
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-30 23:18:06 +0200

    add connection

--- backend/service-privateroom/models/Conversation.js
+++ backend/service-privateroom/models/Conversation.js
@@ -1,4 +1,12 @@
-const mongoose = require("mongoose")
+const config = require('../config');
+const mongoose = require( 'mongoose' );
+const url = config.mongodbHost+config.mongodbDatabase;
+
+mongoose.connect(url,({useNewUrlParser: true, useUnifiedTopology: true})).then( function(){
+    console.log('mongodb-privated-room connected '+mongoose.connection.readyState);
+}).catch(function(err){
+    console.log('error : '+err);
+});
 
 const ConversationSchema = new mongoose.Schema(
     {
