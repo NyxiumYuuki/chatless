ca057706a5556ae094396dc4592417ce43bb1b32
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-30 23:17:53 +0200

    change port and add conversations documents

--- backend/service-privateroom/config.js
+++ backend/service-privateroom/config.js
@@ -1,12 +1,13 @@
 const config = {
     mongodbDatabase: 'chat',
-    mongodbHost:     'mongodb://127.0.0.1:27017/',
+    mongodbHost:     'mongodb://127.0.0.1:27021/',
     // mongodbHost:     'mongodb://127.0.0.1:27018/', //when commit
     charset:       'utf8',
     mongodbLogin:  '',
     mongodbPassword: '',
 
-    mongodbPrivatedMessages: 'privatedMessages'
+    mongodbPrivatedMessages: 'privatedMessages',
+    mongodbConversations: 'conversations'
 };
 module.exports = config;
 
