7b351d8b095e80ad221a492e1f5bfde393050af7
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-05-30 23:16:50 +0200

    add private room

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   urlCL: 'http://127.0.0.1:3000',
-  urlCG: 'http://127.0.0.1:3001'
+  urlCG: 'http://127.0.0.1:3001',
+  urlCPR: 'http://127.0.0.1:3002'
 };
 
 /*
