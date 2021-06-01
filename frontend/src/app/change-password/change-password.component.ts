6c94581615ccd35718be195710c597e0ce2c26f7
NyxiumYuuki <ATAWV5n2q8KG3kn>
2021-06-01 22:35:37 +0200

    changed password added (not verify but should work)

--- frontend/src/app/change-password/change-password.component.ts
+++ frontend/src/app/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MessageService} from "../services/message/message.service";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  username = sessionStorage.getItem('login');
+  oldPassword = '';
+  newPassword = '';
+  confirmPassword = '';
+  errorMessage = '';
+  succesMessage = '';
+
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
+  changePassword(): void {
+    console.log(this.username);
+    if(this.newPassword !== this.confirmPassword){
+      this.errorMessage = 'Les mots de passe ne sont pas identiques.';
+    }
+    else {
+      this.messageService.sendMessage(environment.urlCL,"changePassword",{username: this.username, password: this.oldPassword, newpassword: this.confirmPassword}).subscribe(data => {
+        if (data.status !== 'ok') {
+          this.succesMessage = '';
+          this.errorMessage = data.data.reason;
+        } else {
+          this.errorMessage = '';
+          this.succesMessage = data.data;
+        }
+      });
+    }
+  }
 }
